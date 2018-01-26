e0aa94563c21b54bd1fe17472484fa5f702057fa
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-01-25 23:35:22 -0500

    Cleans out test with new black spec.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+  it('should have `th` "Items"', () => {
+  // Assert
+  });
+}); 
