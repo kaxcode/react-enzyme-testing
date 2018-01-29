e80b62c5f29e887e0823bd96567ada2ddf666b75
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-01-28 23:37:16 -0500

    Adss remainin assertions.

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import App from './App';
-
 import React from 'react';
 import { shallow } from 'enzyme';
 
@@ -7,25 +6,38 @@ describe('App', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<App />);
+    wrapper = shallow(
+      <App />
+    );
   });
 
-  it('should have `button` element', () => {
-    expect(wrapper
-      .containsMatchingElement(<button>Add item</button>))
-      .toBe(true);
+  it('should have the `th` "Items"', () => {
+    expect(
+      wrapper.contains(<th>Items</th>)
+    ).toBe(true);
+  });
+
+  it('should have a `button` element', () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <button>Add item</button>
+      )
+    ).toBe(true);
+  });
+
+  it('should have an `input` element', () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <input />
+      )
+    ).toBe(true);
   });
 
   it('`button` should be disabled', () => {
     const button = wrapper.find('button').first();
-    expect(button.props().disabled)
-      .toBe(true);
-  });
-
-  it('should have `input` element', () => {
-    expect(wrapper
-      .containsMatchingElement(<input />))
-      .toBe(true);
+    expect(
+      button.props().disabled
+    ).toBe(true);
   });
 
   describe('the user populates the input', () => {
@@ -37,5 +49,18 @@ describe('App', () => {
         target: { value: item }
       })
     });
+
+    it('should update the state property `item`', () => {
+      expect(
+        wrapper.state().item
+      ).toEqual(item);
+    });
+
+    it('should enable `button`', () => {
+      const button = wrapper.find('button').first();
+      expect(
+        button.props().disabled
+      ).toBe(false);
+    });
   });
-}); 
+});
